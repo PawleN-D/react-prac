ddcca02f062df8513a7a58ef4a726289fa89eb15
PawleN-D <sirpawle@gmail.com>
2020-05-29 02:58:38 +0200

    added the handleSubmit method to the App class

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component }from 'react';
-import Table from './Table'
+import Table from './Table';
+import Form from './Form';
 
 class App extends Component {
 
@@ -17,16 +18,21 @@ class App extends Component {
     });
   };
 
+  handleSubmit = character => {
+    this.setState({ characters: [...this.state.characters, character] });
+  };
+
   render() {
     const { characters } = this.state;
 
     return (
       <div className="container">
         <Table characterData = {characters} removeCharacter={this.removeCharacter}/>
+        <Form handleSubmit={this.handleSubmit} />
       </div>
-    )
+    );
   }
-}
+};
 
 
 export default App;
